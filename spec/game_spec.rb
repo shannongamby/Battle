9175cbc90b530bdb50d3ecfdd545098c9dc3b52b
shannongamby <shannongamby@hotmail.com>
2018-10-18 12:33:37 +0100

    skinny controller

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,10 +1,20 @@
 require 'game'
 
 describe Game do
-  let(:player_2) { double :player_2 }
+  let(:player_1) { double :player }
+  let(:player_2) { double :player }
+  subject(:game) { described_class.new(player_1, player_2) }
+
+  it "should present player 1" do
+    expect(game.player_1).to eq player_1
+  end
+
+  it "should present player 2" do
+    expect(game.player_2).to eq player_2
+  end
 
   it "should damage the player" do
     expect(player_2).to receive(:decrease_hp)
-    subject.attack(player_2)
+    game.attack(player_2)
   end
 end
